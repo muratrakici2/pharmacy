4825f56da3b6681c823d6e65fb4ef724ca8e4f4d
Murat Rakıcı <muratrakici2@gmail.com>
2022-04-06 14:29:41 +0300

    useref alert

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react'
-import { MedicineContext } from './Context'
+import React, { useContext, useRef } from "react";
+import { MedicineContext } from "./Context";
 
-
-
-const Item = ({ item,alert }) => {
-    const context = useContext(MedicineContext);
-    const add = (e) =>{
-        alert();
-        context.addCart(e);
-    }
-
-
-    return (
+const Item = ({ items }) => {
+  const context = useContext(MedicineContext);
+  const myAlert = useRef(null);
+  const add = (e) => {
+    context.addCart(e);
+    myAlert.current.classList.remove("animasyon");
+    setTimeout(() => {
+      myAlert.current.classList.add("animasyon");
+    }, 1);
+  };
+ 
+  return (
+    <React.Fragment>
+      <div ref={myAlert} className="myalert">
+        Ürün Sepete Eklendi &#10003;
+      </div>
+      {items.map((item) => (
         <div className="medicine" key={item.id}>
-            <h4>{item.name}</h4>
-            <p>{item.brand}</p>
-            <h5>{item.price} ₺</h5>
-            <button onClick={() => add(item)}>Sepete Ekle</button>
+          <h4>{item.name}</h4>
+          <p>{item.brand}</p>
+          <h5>{item.price} ₺</h5>
+          <button onClick={() => add(item)}>Sepete Ekle</button>
         </div>
-    )
-}
+      ))}
+    </React.Fragment>
+  );
+};
 
-export default Item
+export default Item;
--- src/components/Medicines.js
+++ src/components/Medicines.js
@@ -1,36 +1,28 @@
-import React, { useState } from 'react'
-import medicine from "../medicine.json"
-import Item from './Item';
+import React, { useState } from "react";
+import medicine from "../medicine.json";
+import Item from "./Item";
 
 const Medicines = () => {
-    const [data, setData] = useState(medicine);
-    const [myAlert, setmyAlert] = useState(false)
+  const [data, setData] = useState(medicine);
 
-    const searchMedicine = (e) => {
-        const search = e.target.value;
-        const filteredUser = medicine.filter((data) => {
-            return data.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
-        });
-        setData(filteredUser);
-    }
-    const cartAlert = () => {
-        setmyAlert(true)
-        setTimeout(() => {
-            setmyAlert(false)
-        }, 2000);
-    }
-    return (
-        <div className="medicines-main">
-            {myAlert&&<div className='myalert'>Ürün Sepete Eklendi &#10003;</div>}
-            <h3>İLAÇLAR</h3>
-            <input type="text" placeholder="İlaç Ara" onChange={searchMedicine} />
-            <div className="medicine-contanier">
-                {data.map((i) => (
-                    <Item key={i.id} item={i} alert={cartAlert} />
-                ))}
-            </div>
-        </div>
-    )
-}
+  const searchMedicine = (e) => {
+    const search = e.target.value;
+    const filteredUser = medicine.filter((data) => {
+      return data.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+    setData(filteredUser);
+  };
+  
+  console.log("medicines çalıştı");
+  return (
+    <div className="medicines-main">
+      <h3>İLAÇLAR</h3>
+      <input type="text" placeholder="İlaç Ara" onChange={searchMedicine} />
+      <div className="medicine-contanier">
+        <Item items={data} />
+      </div>
+    </div>
+  );
+};
 
-export default Medicines
+export default Medicines;
